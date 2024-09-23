2bd0dbf0f0eebf582f9228a4303d3d839ead46f6
OliviaLim123 <131115382+OliviaLim123@users.noreply.github.com>
2024-09-23 20:16:19 +1000

    Set up the Xcode Project
    Creating the Xcode Project for assignment 3

---
+++ MovieHunter/MovieHunter/MovieHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieHunterApp.swift
+//  MovieHunter
+//
+//  Created by Olivia Gita Amanda Lim on 23/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
