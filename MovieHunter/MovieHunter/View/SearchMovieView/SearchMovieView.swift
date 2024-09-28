ae0ac381f7dc7cb68a34886268af709881780d87
OliviaLim123 <131115382+OliviaLim123@users.noreply.github.com>
2024-09-28 21:55:56 +1000

    Created the search bar view
    But it is not function properly - need to fix it soon

--- MovieHunter/MovieHunter/View/SearchMovieView/SearchMovieView.swift
+++ MovieHunter/MovieHunter/View/SearchMovieView/SearchMovieView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct SearchMovieView: View {
+    @ObservedObject var movieSearchVM = MovieSearchViewModel()
+    
     var body: some View {
-        Text("Search Movie")
+        NavigationView {
+           ScrollView {
+                SearchBarView(placeholder: "Search movies", text: self.$movieSearchVM.query)
+                    .padding(.horizontal, 10)
+                
+                LottieView(name: Constants.loadingAnimation, loopMode: .loop, animationSpeed: 1.0) {
+                    self.movieSearchVM.search(query: self.movieSearchVM.query)
+                }
+                
+                if self.movieSearchVM.movies != nil {
+                    if let movies = self.movieSearchVM.movies {
+                        ForEach(movies) { movie in
+                            NavigationLink(destination: MovieDetailView(movieId: movie.id)) {
+                                VStack(alignment: .leading) {
+                                    Text(movie.title)
+                                    Text(movie.yearText)
+                                }
+                            }
+                        }
+                    } else {
+                        Text("No movies are found.")
+                    }
+                }
+            }
+            .onAppear {
+                self.movieSearchVM.startObserve()
+            }
+            .navigationBarTitle("Search Movies")
+        }
     }
 }
 
