7c8c73c936ba7728953f6e3aeb4d3e9dbbf43780
OliviaLim123 <131115382+OliviaLim123@users.noreply.github.com>
2024-10-06 20:33:12 +1100

    Update TranscribedTextFieldView.swift

--- MovieHunter/MovieHunter/SpeechToText/TranscribedTextFieldView.swift
+++ MovieHunter/MovieHunter/SpeechToText/TranscribedTextFieldView.swift
@@ -6,16 +6,23 @@
 //
 
 import SwiftUI
+
+// MARK: TRANSCRIBED TEXT FIELD VIEW
 public struct TranscribedTextFieldView: View {
     
+    // STATE PROPERTIES of TranscribedTextFieldView
     @State var color:Color
     @State var isRecording:Bool
+    
+    // BINDING PROPERTY
     @Binding var searchText:String
-    // default values
+    
+    // PROPERTIES
     var placeHolder:String
     var micIconWidth:CGFloat
     var micIconHeight:CGFloat
     
+    // INIT FUNCTION to initialise the text field
     public init(color: Color = Color("customBlack"), isRecording: Bool = false, searchText: Binding<String>, placeHolder: String = "Please enter movie title", micIconWidth: CGFloat = 12, micIconHeight: CGFloat = 20) {
         self.color = color
         self.isRecording = isRecording
@@ -25,26 +32,29 @@ public struct TranscribedTextFieldView: View {
         self.micIconHeight = micIconHeight
     }
     
-    public var body: some View{
-        HStack{
+    // BODY VIEW
+    public var body: some View {
+        HStack {
+            // Display the text field
             TextField("", text: $searchText,prompt: Text(placeHolder))
                 .textFieldStyle(RoundedBorderTextFieldStyle())
+            // Display the microphone icon
             Image(systemName: "mic.fill")
                 .resizable()
                 .frame(width: micIconWidth, height: micIconHeight)
                 .foregroundStyle( color )
+            // If the microphone is clicked, it will start to record the voice
                 .onTapGesture {
-//                    isRecording.toggle()
                     if !isRecording {
-                                // Ensure any ongoing session is stopped before starting
-                                isRecording = true
-                            }
+                        isRecording = true
+                    }
                 }
         }
-        .sheet(isPresented: $isRecording){
-                SwiftUIView(isRecording: self.$isRecording, searchText: self.$searchText, color: self.$color)
-                    .presentationDetents([.medium, .fraction(0.5)])
-                    .presentationDragIndicator(.visible)
-            }
+        // Display the bottom sheet view (recording view)
+        .sheet(isPresented: $isRecording) {
+            SwiftUIView(isRecording: self.$isRecording, searchText: self.$searchText, color: self.$color)
+                .presentationDetents([.medium, .fraction(0.5)])
+                .presentationDragIndicator(.visible)
+        }
     }
 }
